6b93527108b1157af0d2d0b4da5250475dcc3f7a
Filipe Silva <silvam.filipe@gmail.com>
2015-03-06 02:00:02 -0100

    Started URI implementation

---
+++ src/Uri.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of HttpMessage package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fsilva\HttpMessage;
+
+
+/**
+ * Class Uri
+ * @package Fsilva\HttpMessage
+ */
+class Uri
+{
+
+}
---
+++ tests/UriTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of HttpMessage package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fsilva\HttpMessage\Tests;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * UriTest test case
+ *
+ * @package Fsilva\HttpMessage\Tests
+ * @author  Filipe Silva <silvam.filipe@gmail.com>
+ */
+class UriTest extends TestCase 
+{
+
+}
